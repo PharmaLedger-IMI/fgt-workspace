69e1f759b789bd37954402afbce0a3e8278ef6ec
Francisco Damiao <francisco.damiao@pdmfc.com>
2021-02-08 15:48:42 +0000

    Added exception handling

--- tests/loadOneDSU.js
+++ tests/loadOneDSU.js
@@ -11,58 +11,65 @@ const resolver = opendsu.loadApi("resolver");
 const keyssispace = opendsu.loadApi("keyssi");
 
 //Create a template keySSI (for default domain). See /conf/BDNS.hosts.json
-const aKeySSI = "ssi:seed:default:637c5f7AYfvLTM76xrQK7bsDe31Ybo9Q7zoP8QSgddYs::v0";
-//const aKeySSI = "ssi:sread:default:cGeb7JqYuVMszUTiw471bcnrEVALTa3F4eDwHjALYsh:F9MnSteh1WCM9RWfrirUe6G5kFb3gvRh8kW9sTunzG18:v0";
-
-//Using an sZaSSI, it is not possible to load
-// const aKeySsi = "ssi:sza:default::EjgYgxE7bEC4tuVdpRiE3RJzepXFLY3u8urAYgeTHVES:v0"
+//const aKeySSI = "ssi:sread:default:GfQ33HRQywE5nD1AuonozFNgjj7yXpK9bGwVR1xapuT4:255nSKKXmg3B4DhQ4BoU2S53odRAuPHxAgSYjGv1sLCR:v0";
+const aKeySSI = "BBudGH6ySHG6GUHN8ogNrTWbvGyG1VRGoheZpyY8Zv3WfY5LBSdPN6hkJwveRNnce1PQEGNpWvTKi7NkLeug9qeTy";
 
 //Load a DSU
-resolver.loadDSU(aKeySSI, (err, aDsuInstance) =>{
-    //Reached when DSU loaded
-    if (err){
-        console.log("DSU loaded BAD");
-        throw err;
-    }
-    console.log("DSU loaded OK");
-
-    aDsuInstance.listFiles("/", function (err,files) {
-        console.log("Files "+files);
-    });
-    aDsuInstance.listFolders("/", function (err,folders) {
-        console.log("Folders "+folders);
-    });
-    
-    aDsuInstance.readFile('/dsu-metadata-log', (err, data)=>{
-        //Reached when data loaded
-        if(err){
+try {
+    resolver.loadDSU(aKeySSI, (err, aDsuInstance) => {
+        //Reached when DSU loaded
+        if (err){
+            console.log("Error loading DSU:");
             throw err;
         }
-        
-        console.log("dsu-metadata-log load succesfully! :", data.toString());
-    });
+        console.log("DSU loaded OK");
 
-    aDsuInstance.readFile('/data', (err, data)=>{
-        //Reached when data loaded
-        if(err){
-            throw err;
-        }
+        aDsuInstance.listFiles("/", function (err,files) {
+            console.log("Files "+files);
+        });
+        aDsuInstance.listFolders("/", function (err,folders) {
+            console.log("Folders "+folders);
+        });
         
-        const dataObject = JSON.parse(data.toString()); //Convert data (buffer) to string and then to JSON
-        console.log("Data load succesfully! :)", dataObject.message); //Print message to console
-        dataObject.message += " ... more!";
-        console.log("Data changed :)", dataObject.message); //Print message to console
-
-        aDsuInstance.writeFile("/data", JSON.stringify(dataObject), (err) => {
-            if (err) {
+        aDsuInstance.readFile('/dsu-metadata-log', (err, data)=>{
+            //Reached when data loaded
+            if(err){
+                console.log("DSU dsu-metadata-log readfile error: ");
                 throw err;
             }
-            console.log("Data written succesfully! :)");
+            
+            console.log("dsu-metadata-log load succesfully! :", data.toString());
         });
+
+        aDsuInstance.readFile('/data', (err, data)=>{
+            //Reached when data loaded
+            if(err){
+                console.log("DSU data readfile error: ");         
+                throw err;
+            }
+            
+            const dataObject = JSON.parse(data.toString()); //Convert data (buffer) to string and then to JSON
+            console.log("Data load succesfully! :) -> ", dataObject.message); //Print message to console
+            dataObject.message += " ... more!";
+            console.log("Data changed :)           -> ", dataObject.message); //Print message to console
+
+            aDsuInstance.writeFile("/data", JSON.stringify(dataObject), (err) => {
+                if (err) {
+                    console.log("DSU write readfile error: ");
+                    console.log( err ); 
+                    throw err;
+                }
+                console.log("Data written succesfully! :)");
+            });
+        });
+
     });
-
-});
-
+} catch (ex ) {
+    
+    console.log("Exception:");
+    console.log( ex );
+    
+}
 
 
 
