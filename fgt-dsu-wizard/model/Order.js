aa24ff763f0b7739d5ebbe8db069abf9977a45d0
jpsl <joao.luis@pdmfc.com>
2021-03-11 10:55:13 +0000

    #1 - Order validate()

--- fgt-dsu-wizard/model/Order.js
+++ fgt-dsu-wizard/model/Order.js
@@ -19,6 +19,43 @@ class Order{
         this.status = status || OrderStatus.CREATED;
         this.orderLines = orderLines || [];
     }
+
+    /**
+     * Validate if everything seems ok with the properties of this object.
+     * @returns undefined if all ok. An arry of errors if not all ok.
+     */
+    validate() {
+        const errors = [];
+        if (!this.orderId) {
+            errors.push('OrderID is required.');
+        }
+        if (!this.requesterId) {
+            errors.push('Ordering partner ID is required.');
+        }
+        if (!this.senderId) {
+            errors.push('Supplying partner ID is required.');
+        }
+        if (!this.shipToAddress) {
+            errors.push('ShipToAddress is required.');
+        }
+        if (!this.status) {
+            errors.push('status is required.');
+        }
+        if (!this.orderLines || this.orderLines.length == 0) {
+            errors.push('orderLines is required.');
+        } else {
+            this.orderLines.forEach((orderLine,orderLineIndex) => {
+                let orderLineErrors = orderLine.validate();
+                if (orderLineErrors) {
+                    orderLineErrors.forEach((error) => {
+                        errors.push("Order Line "+orderLineIndex+": "+error);
+                    });
+                }
+            });
+        }
+
+        return errors.length === 0 ? undefined : errors;
+    }
 }
 
 module.exports = Order;
