6c71ff76de30ddc3c504b20cbfbf9d4f7475ce5f
jpsl <joao.luis@pdmfc.com>
2021-03-19 18:36:25 +0000

    #2 -  deployDev.sh should work buts needs more tests

--- docker/util/deployDev.sh
+++ docker/util/deployDev.sh
@@ -32,23 +32,24 @@ fi
 
 # Image .tar name based on current date and time
 #IMG_NAME=fgtYYYYMMDDHHMISS
-IMG_NAME=${UCNAME}$(date +%Y%m%d%H%M%S).tar
+IMG_NAME=${UCNAME}$(date +%Y%m%d%H%M%S).tar.xz
 
 # Remove tmp file on exit
-trap "rm -f /tmp/$IMG_NAME.xz" EXIT
+trap "rm -f /tmp/$IMG_NAME" EXIT
 
 # Saving and compressimg docker $UCNAME to /tmp/$IMG_NAME.xz. (Use all CPU threads when compressing).
-docker save pharmaledger/$UCNAME | xz -T0 > /tmp/$IMG_NAME.xz
+docker save pharmaledger/$UCNAME | xz -T0 > /tmp/$IMG_NAME
 
 # uploading /tmp/$IMG_NAME.xz to $USERATHOST:images/
-scp -p /tmp/$IMG_NAME.xz $USERATHOST:images/
+scp -p /tmp/$IMG_NAME $USERATHOST:images/
 
 # stop and delete all running images. Then load and start the new one.
 ssh $USERATHOST <<EOF
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
-( xz -d < images/$IMG_NAME.xz | docker load )
+set -e
+docker stop \$(docker ps -aq)
+docker rm \$(docker ps -aq)
+docker rmi \$(docker images -q)
+( xz -d < images/$IMG_NAME | docker load )
 docker run --detach --hostname fgt --publish 8080:8080 --name $UCNAME --restart always pharmaledger/fgt
 docker logs -f fgt
 EOF
