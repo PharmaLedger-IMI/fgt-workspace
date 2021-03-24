929578e95784f99f0272a3246f999b744100d35c
jpsl <joao.luis@pdmfc.com>
2021-03-24 17:21:15 +0000

    #1 - docker start.sh and run.sh are different scripts

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 UCNAME="fgt"
 
-docker run --detach \
-    --hostname $UCNAME \
-    --publish 8080:8080 \
-    --name $UCNAME \
-    --restart always \
-    pharmaledger/$UCNAME
+docker start $UCNAME
