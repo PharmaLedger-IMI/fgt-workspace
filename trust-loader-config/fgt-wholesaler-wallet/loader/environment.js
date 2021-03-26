16455f4bfb99e8ab2671afae4affae74c7b2f649
Tiago <tiago.venceslau@pdmfc.com>
2021-03-26 00:57:45 +0000

    refs #1 - reserts to webcardinal bundler. refactor to include pdm loader in wholesaler app oin navtive build

--- trust-loader-config/fgt-wholesaler-wallet/loader/environment.js
+++ trust-loader-config/fgt-wholesaler-wallet/loader/environment.js
@@ -5,9 +5,10 @@ export default {
   "system":   "any",
   "browser":  "any",
   "mode":  "dev-secure",
-  "domain":  "vault",
+  "domain":  "traceability",
   "sw": true,
   "pwa": true,
-  "basePath": "fgt-wholesaler-wallet/loader",
+  "basePath": "/pdm-trust-loader/loader/",
+  "theme": "pdm-theme",
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false) pwa:(true, false)"
 }
