e18e8dbe757755671d3edcbef09cb74daedbeed4
Tiago <tiago.venceslau@pdmfc.com>
2021-04-14 17:14:05 +0100

    refs #1 - improving env recreate script

--- bin/environment/createWholesaler.js
+++ bin/environment/createWholesaler.js
@@ -5,7 +5,7 @@ const path = require('path');
 require(path.join('../../privatesky/psknode/bundles', 'openDSU.js'));       // the whole 9 yards, can be replaced if only
 const dt = require('./../../pdm-dsu-toolkit/services/dt');
 const { getParticipantManager, getStockManager, getBatchManager } = require('../../fgt-dsu-wizard/managers');
-const { impersonateDSUStorage } = require('./utils');
+const { impersonateDSUStorage, argParser, instantiateSSApp } = require('./utils');
 const {Stock, Product, Batch} = require('../../fgt-dsu-wizard/model');
 
 const defaultOps = {
@@ -14,83 +14,9 @@ const defaultOps = {
     id: undefined
 }
 
-const argParser = function(args){
-    let config = JSON.parse(JSON.stringify(defaultOps));
-    if (!args)
-        return config;
-    args = args.slice(2);
-    const recognized = Object.keys(config);
-    const notation = recognized.map(r => '--' + r);
-    args.forEach(arg => {
-        if (arg.includes('=')){
-            let splits = arg.split('=');
-            if (notation.indexOf(splits[0]) !== -1) {
-                let result
-                try {
-                    result = eval(splits[1]);
-                } catch (e) {
-                    result = splits[1];
-                }
-                config[splits[0].substring(2)] = result;
-            }
-        }
-    });
-    return config;
-}
+let conf = argParser(defaultOps, process.argv);
 
-let conf = argParser(process.argv);
-
-const generateSecrets = function(id) {
-    return {
-        "name": {
-            "value": "PDM the Wholesaler",
-            "public": true,
-            "required": true
-        },
-        "id": {
-            "value": id,
-            "public": true,
-            "required": true
-        },
-        "email": {
-            "value": "wholesaler@pdmfc.com",
-            "public": true,
-            "required": true
-        },
-        "tin": {
-            "value": 500000000,
-            "public": true,
-            "required": true
-        },
-        "address": {
-            "required": true,
-            "value": "This in an Address"
-        },
-        "pass": {
-            "required": true,
-            "value": "This1sSuchAS3curePassw0rd"
-        },
-        "passrepeat": {
-            "required": true,
-            "value": "This1sSuchAS3curePassw0rd"
-        },
-    }
-};
-
-const parseEnvJS = function(strEnv){
-    return JSON.parse(strEnv.replace(/^export\sdefault\s/, ''));
-}
-
-const getEnvJs = function(app, callback){
-    const appPath = path.join(process.cwd(), conf.pathToApps, "trust-loader-config", app, "loader", "environment.js");
-    require('fs').readFile(appPath, (err, data) => {
-        if (err)
-            return callback(`Could not find Application ${app} at ${{appPath}} : ${err}`);
-        return callback(undefined, parseEnvJS(data.toString()));
-    });
-}
-
-const setupStocks = function(participantManager, products, batches, stocks, callback){
+const setup = function(participantManager, products, batches, stocks, callback){
     if (!callback){
         callback = stocks;
         stocks = undefined;
@@ -147,28 +73,7 @@ const setupStocks = function(participantManager, products, batches, stocks, call
     })
 }
 
-const instantiateSSApp = function(callback){
-    getEnvJs(conf.app, (err, env) => {
-        if (err)
-            throw err;
-
-        let config = require("opendsu").loadApi("config");
-        config.autoconfigFromEnvironment(env);
-
-        const appService = new (dt.AppBuilderService)(env);
-        const id = conf.id || Math.round(Math.random() * 999999999);
-        console.log(`Generating ${conf.app} with ID: ${id}`);
-        const credentials = generateSecrets(id);
-        appService.buildWallet(credentials, (err, keySII, dsu) => {
-            if (err)
-                throw err;
-            console.log(`App ${env.appName} created with credentials ${JSON.stringify(credentials, undefined, 2)}.\nSSI: ${{keySII}}`);
-            callback(undefined, keySII, dsu, credentials);
-        });
-    });
-}
-
-const createWholesaler = function(products, batches, stock, callback){
+const create = function(credentials, callback){
     instantiateSSApp((err, walletSSI, walletDSU, credentials) => {
         if (err)
             throw err;
@@ -176,20 +81,19 @@ const createWholesaler = function(products, batches, stock, callback){
         getParticipantManager(dsuStorage, (err, participantManager) => {
             if (err)
                 throw err;
-            setupStocks(participantManager, products, batches,(err, stocks) => {
-                if (err)
-                    throw err;
-                console.log(`${conf.app} instantiated\ncredentials:`);
-                console.log(credentials)
-                console.log(`ID: ${credentials.id.secret}`);
-                participantManager.shutdownMessenger();
-                callback(undefined, credentials, products, batches, stocks);
-            });
+
+            console.log(`${conf.app} instantiated\ncredentials:`);
+            console.log(credentials)
+            console.log(`ID: ${credentials.id.secret}`);
+            callback(undefined, credentials, walletSSI, participantManager);
         });
     });
 }
 
-module.exports = createWholesaler;
+module.exports = {
+    create,
+    setup
+};
 
 
 
---
+++ bin/environment/credentials/credentials.js
@@ -0,0 +1,176 @@
+const generateWholesalerCredentials = function(id) {
+    return {
+        "name": {
+            "value": "PDM the Wholesaler",
+            "public": true,
+            "required": true
+        },
+        "id": {
+            "value": id,
+            "public": true,
+            "required": true
+        },
+        "email": {
+            "value": "wholesaler@pdmfc.com",
+            "public": true,
+            "required": true
+        },
+        "tin": {
+            "value": 500000000,
+            "public": true,
+            "required": true
+        },
+        "address": {
+            "required": true,
+            "value": "This in an Address"
+        },
+        "pass": {
+            "required": true,
+            "value": "This1sSuchAS3curePassw0rd"
+        },
+        "passrepeat": {
+            "required": true,
+            "value": "This1sSuchAS3curePassw0rd"
+        },
+    }
+};
+
+const generatePharmacyCredentials = function(id) {
+    return {
+        "name": {
+            "secret": "PDM the Pharmacy",
+            "public": true,
+            "required": true
+        },
+        "id": {
+            "secret": id,
+            "public": true,
+            "required": true
+        },
+        "email": {
+            "secret": "pharmacy@pdmfc.com",
+            "public": true,
+            "required": true
+        },
+        "tin": {
+            "secret": 500000000,
+            "public": true,
+            "required": true
+        },
+        "address": {
+            "required": true,
+            "secret": "This in an Address"
+        },
+        "pass": {
+            "required": true,
+            "secret": "This1sSuchAS3curePassw0rd"
+        },
+        "passrepeat": {
+            "required": true,
+            "secret": "This1sSuchAS3curePassw0rd"
+        }
+    }
+};
+
+const generateMAHCredentials = function(id) {
+    return {
+        "name": {
+            "secret": "PDM the MAH",
+            "public": true,
+            "required": true
+        },
+        "id": {
+            "secret": id,
+            "public": true,
+            "required": true
+        },
+        "email": {
+            "secret": "mah@pdmfc.com",
+            "public": true,
+            "required": true
+        },
+        "tin": {
+            "secret": 500000000,
+            "public": true,
+            "required": true
+        },
+        "address": {
+            "required": true,
+            "secret": "This in an Address"
+        },
+        "pass": {
+            "required": true,
+            "secret": "This1sSuchAS3curePassw0rd"
+        },
+        "passrepeat": {
+            "required": true,
+            "secret": "This1sSuchAS3curePassw0rd"
+        }
+    }
+};
+
+const APPS = {
+    MAH: 'fgt-mah-wallet',
+    WHOLESALER: 'fgt-wholesaler-wallet',
+    PHARMACY: 'fgt-pharmacy-wallet',
+    FULL: 'full',
+    PROD: 'prod'
+}
+
+const MERKL = {
+    "name": {
+        "secret": "Merkl",
+        "public": true,
+        "required": true
+    },
+    "id": {
+        "secret": 'MERKL id',
+        "public": true,
+        "required": true
+    },
+    "email": {
+        "secret": "mah@merkl.com",
+        "public": true,
+        "required": true
+    },
+    "tin": {
+        "secret": 1234566789,
+        "public": true,
+        "required": true
+    },
+    "address": {
+        "required": true,
+        "secret": "merkl's address"
+    },
+    "pass": {
+        "required": true,
+        "secret": "MerklPassw0rd"
+    },
+    "passrepeat": {
+        "required": true,
+        "secret": "MerklPassw0rd"
+    }
+}
+
+const PROD = {}
+PROD[APPS.MAH] = [MERKL]
+
+const getCredentials = function(type, reference){
+    if (typeof reference === 'string')
+        return PROD[type][reference];
+    switch (type){
+        case APPS.WHOLESALER:
+            return generateWholesalerCredentials(Math.floor(Math.random() * 999999999));
+        case APPS.MAH:
+            return generateMAHCredentials(Math.floor(Math.random() * 999999999));
+        case APPS.PHARMACY:
+            return generatePharmacyCredentials(Math.floor(Math.random() * 999999999));
+        default:
+            throw new Error(`invalid request`);
+    }
+}
+
+module.exports = {
+    getCredentials,
+    APPS
+}
