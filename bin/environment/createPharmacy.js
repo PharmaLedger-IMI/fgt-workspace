b8e03f5ee24e7686fc575a985eb77172358e5f10
jpsl <joao.luis@pdmfc.com>
2021-04-15 14:30:47 +0100

    #10 - createPharmacy.js setup creates an issuedOrder - not working

--- bin/environment/createPharmacy.js
+++ bin/environment/createPharmacy.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 require(path.join('../../privatesky/psknode/bundles', 'openDSU.js'));       // the whole 9 yards, can be replaced if only
 const dt = require('../../pdm-dsu-toolkit/services/dt');
-const { getParticipantManager, getProductManager, getBatchManager } = require('../../fgt-dsu-wizard/managers');
+const { getParticipantManager, getIssuedOrderManager } = require('../../fgt-dsu-wizard/managers');
+const { Order, OrderLine } = require('../../fgt-dsu-wizard/model');
 const { impersonateDSUStorage, argParser, instantiateSSApp } = require('./utils');
 
 const { APPS } = require('./credentials/credentials');
@@ -18,8 +19,34 @@ const defaultOps = {
 let conf = argParser(process.argv);
 
 
-const setup = function (participant, issuedOrders, receivedShipments, callback) {
-    callback(undefined, issuedOrders, receivedShipments);
+const setup = function (participantManager, products, wholesalers, stocks, callback) {
+    // TODO move this to a function that generates issuedOrders ?
+    const issuedOrderManager = getIssuedOrderManager(participantManager);
+
+    let issuedOrders = [];
+
+    let receivedShipments = [];
+
+    // Order#1 - products[0] ordered to wholesalers[0]
+    if (products.length <=0)
+        return callback("Products has zero length.");
+    if (wholesalers.length <=0)
+        return callback("Wholesalers has zero length.");
+    const product0 = product[0];
+    issuedOrderManager.newBlank((err, order) => {
+        if (err)
+            return callback(err);
+        order.senderId = wholesalers[0].id;
+        const orderLine1 = new OrderLine(product0.gtin, 1, order.requesterId, order.senderId);
+        order.orderLines.push(orderLine1);
+        issuedOrderManager.create(order, (err, keySSI) => {            
+            if (err)
+                return callback(err);
+            issuedOrders.push(order);
+            // last step
+            callback(undefined, issuedOrders, receivedShipments);
+        });
+    });
 }
 
 const create = function (credentials, callback) {
