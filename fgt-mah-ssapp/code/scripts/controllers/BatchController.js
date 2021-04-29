0326773595a10f7e3773e48b792213ae51f58642
Tiago <tiago.venceslau@pdmfc.com>
2021-04-29 11:52:19 +0100

    refs #10 - adds query to table

--- fgt-mah-ssapp/code/scripts/controllers/BatchController.js
+++ fgt-mah-ssapp/code/scripts/controllers/BatchController.js
@@ -1,18 +1,33 @@
 import {LocalizedController} from "../../assets/pdm-web-components/index.esm";
-const Managers = require('wizard').Managers
 
+/**
+ * Controls Application Flow
+ * Makes the bridge between the UI and the BatchManager
+ *
+ * Handles data input and validation for the manipulation of Batches
+ * @class BatchController
+ * @module controllers
+ */
 export default class BatchController extends LocalizedController {
+
+    initializeModel = () => ({});
+
     constructor(element, history) {
         super(element, history, false);
-        const LocaleService = require('wizard').Services.WebcLocaleService;
-        LocaleService.bindToLocale(this, "batch", true);
-        let participantManager = Managers.getParticipantManager();
-        this.batchManager = Managers.getBatchManager(participantManager);
-        this.on('submit-batch', this._handleSubmit.bind(this));
+        let self = this;
+        const wizard = require('wizard').Managers
+        super.bindLocale(this, "batch", true);
+        let participantManager = wizard.Managers.getParticipantManager();
+        this.batchManager = wizard.Managers.getBatchManager(participantManager);
+        this.model = self.initializeModel();
+        self.onTagClick(`try-create-batch`, self._submitBatch.bind(self));
     }
 
-    _handleSubmit(event){
-        let batch = this.batchManager.fromModel(this.model);
-        this.send('perform-add-batch', batch);
+    _submitBatch = function () {
+        let self = this;
+        if (self.hasErrors(true))
+            return this.showErrorToast('There are errors in the form');
+        const product = self.batchManager.fromModel(self.model);
+        this.send('create-batch', product);
     }
 }
