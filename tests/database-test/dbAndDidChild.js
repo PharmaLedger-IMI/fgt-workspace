7e29b364d163c8d564b137c8e20ac1bb26259cb8
Tiago <tiago.venceslau@pdmfc.com>
2021-05-13 18:22:30 +0100

    refs #10 - finishes tests

--- tests/database-test/dbAndDidChild.js
+++ tests/database-test/dbAndDidChild.js
@@ -5,23 +5,31 @@ require("../../privatesky/psknode/bundles/openDSU");
 const opendsu = require("opendsu");
 const w3cDID = opendsu.loadApi('w3cdid');
 
-process.on('message', (id) => {
-    w3cDID.createIdentity("demo", id, (err, did) => {
+process.on('message', (args) => {
+    const {id, didMethod, messages} = args;
+    let messageCount = 0;
+    w3cDID.createIdentity(didMethod, id, (err, did) => {
         if (err) {
             throw err;
         }
         console.log(`${did.getIdentifier()} waiting for messages`);
         const listen = function(){
+            console.log(`Listening for messages on ${did.getIdentifier()}`);
             did.readMessage((err, msg) => {
                 if(err){
-                    console.log(`ERROR: ${err}`);
+                    console.log(`ERROR:`, err);
                     return listen();
                 }
-                console.log(`${did.getIdentifier()} received message: ${msg}`);
+                console.log(`${did.getIdentifier()} received message: ${JSON.stringify(msg)}`);
+                if (++ messageCount === messages){
+                    console.log(`Received all ${messages} expected messages. Shutting Down...`)
+                    process.exit(0)
+                }
+
                 listen();
             });
         }
         listen();
-        process.send(id);
+        process.send(did.getIdentifier());
     });
 })
