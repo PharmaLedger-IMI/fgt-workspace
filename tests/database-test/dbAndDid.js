a8160d949c2029a7bfc95c94c9c03ec9a4d6ee51
Tiago <tiago.venceslau@pdmfc.com>
2021-05-13 18:22:30 +0100

    refs #10 - finishes tests

--- tests/database-test/dbAndDid.js
+++ tests/database-test/dbAndDid.js
@@ -23,98 +23,60 @@ const config = argParser(defaultOps, process.argv)
 
 let msgCount = 0;
 
-let timeBeforeMessages, timeAfterMessages, timeBeforeLoad, timeAfterLoad, timeMessagesSent;
+let timeBeforeMessages, timeAfterMessages, timeMessagesSent;
 
+const someData = {
+    key1: 'value',
+    key2: 'looooooooooooooooooooooonger vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaalue'
+}
 
-    w3cDID.createIdentity(config.didMethod, config.sender, (err, senderDID) => {
-        if (err)
-            throw err;
+w3cDID.createIdentity(config.didMethod, config.sender, (err, senderDID) => {
+    if (err)
+        throw err;
+    const forked = fork('dbAndDidChild.js');
+    forked.on('message', (receiverDID) => {
+        console.log(`received created and listening`);
+        if (config.kill){
+           forked.kill('SIGINT');
+           console.log(`received process shutdown`);
+        }
 
-        const resolver = opendsu.loadApi('resolver');
-        const keyssi = opendsu.loadApi('keyssi');
+        const sendMessage = function(receiver){
+           console.log("Sending message", JSON.stringify(someData), " to receiver ", config.receiver);
+           senderDID.sendMessage(JSON.stringify(someData), receiver.getIdentifier(),  (err) => {
+               if (err)
+                   return console.log(`Error sending message`, err);
+               msgCount++;
+               console.log(`Message successfully sent ${msgCount}`);
+               if (msgCount === config.messages){
+                   timeMessagesSent = Date.now();
+                   console.log(`all messages sent in ${timeMessagesSent - timeAfterMessages}ms. closing test in 3 second`)
+                   setTimeout(() => process.exit(0), 3000);
+               }
+           });
+        }
 
-        const dsuKey = keyssi.createTemplateSeedSSI(config.domain, 'somestring', undefined, 'v0', undefined)
-        resolver.createDSU(dsuKey, (err, dsu) => {
+        const runTest = function(){
+           timeBeforeMessages = Date.now();
+           console.log(`Before Messages: ${timeBeforeMessages}`);
+           w3cDID.createIdentity(config.didMethod, config.receiver, (err, receiver) => {
+               if(err)
+                   throw err;
 
-            const someData = {
-                key1: 'some value',
-                key2: 'other value'
-            }
+               for (let i = 0; i < config.messages; i++)
+                   sendMessage(receiver);
 
-            dsu.writeFile('/info', JSON.stringify(someData), (err) => {
-                if (err)
-                    throw err;
-               dsu.getKeySSIAsObject((err, keySSI) =>{
-                   if (err)
-                       throw err;
-                   dsu = undefined;
+               timeAfterMessages = Date.now();
+               console.log(`After Messages: ${timeAfterMessages}. Elapsed: ${timeAfterMessages - timeBeforeMessages}`);
+           });
+        }
 
-                   // const forked = fork('dbAndDidChild.js');
-                   // forked.on('message', (receiverDID) => {
-                   //     console.log(`received created and listening`);
-                   //     if (config.kill){
-                   //         forked.kill('SIGINT');
-                   //         console.log(`received process shutdown`);
-                   //     }
-
-                       const sendMessage = function(){
-                           console.log("Sending message", JSON.stringify(someData), " to receiver ", config.receiver);
-                           senderDID.sendMessage(JSON.stringify(someData), config.receiver /*receiverDID*/,  (err) => {
-                               if (err)
-                                   return console.log(`Error sending message`, err);
-
-                               console.log(`Message successfully sent`);
-                               msgCount++;
-                               if (msgCount === config.messages){
-                                   timeMessagesSent = Date.now();
-                                   console.log(`all messages sent in ${timeMessagesSent - timeAfterMessages}ms. closing test in 1 second`)
-                                   setTimeout(() => process.exit(0), 1000);
-                               }
-                           });
-                       }
-
-                       const runTest = function(){
-                           timeBeforeMessages = Date.now();
-                           console.log(`Before Messages: ${timeBeforeMessages}`);
-
-                           for (let i = 0; i < config.messages; i++)
-                               sendMessage();
-
-                           timeAfterMessages = Date.now();
-                           console.log(`After Messages: ${timeAfterMessages}. Elapsed: ${timeAfterMessages - timeBeforeMessages}`);
-
-                           timeBeforeLoad = Date.now();
-                           console.log(`Before DSU load ${timeBeforeLoad}`);
-
-                           resolver.loadDSU(keySSI, (err, dsu) => {
-                               if (err)
-                                   throw err;
-
-                               timeAfterLoad = Date.now();
-                               console.log(`After DSU load ${timeAfterLoad}. elapsed: ${timeAfterLoad - timeBeforeLoad}`);
-
-                               dsu.readFile('/info', (err, data) => {
-                                   if (err)
-                                       throw err;
-
-                                   console.log(`Time to read: ${Date.now() - timeAfterLoad}`);
-                                   const result = JSON.parse(data);
-                                   console.log(`Finished:`, result);
-                               });
-                           });
-                       }
-
-                       if (!config.timeout)
-                           return runTest();
-
-                       console.log(`Waiting for ${config.timeout} to run the test...`)
-                       setTimeout(() => {
-                           runTest();
-                       }, config.timeout);
-                   });
-               //
-               //     forked.send(config.receiver);
-               // });
-            });
-        });
+        runTest();
     });
+
+    forked.send({
+        id: config.receiver,
+        didMethod: config.didMethod,
+        messages: config.messages
+    });
+});
