0f07543e4815a51650085de661b65fdafeb70f13
Tiago <tiago.venceslau@pdmfc.com>
2021-05-28 11:23:32 +0100

    refs #13 - removes obsolete code

--- fgt-dsu-wizard/services/index.js
+++ fgt-dsu-wizard/services/index.js
@@ -3,8 +3,6 @@
  */
 module.exports = {
     DSUService: require('../../pdm-dsu-toolkit/services/DSUService'),
-    ParticipantService: require('./ParticipantService'),
-    InboxService: require("./InboxService"),
     WebcLocaleService: require("../../pdm-dsu-toolkit/services/WebcLocaleService"),
     WebComponentService: require("../../pdm-dsu-toolkit/services/WebComponentService"),
     OrderLineService: require("./OrderLineService"),
