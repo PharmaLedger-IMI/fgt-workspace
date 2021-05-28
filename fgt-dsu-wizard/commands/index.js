82b9998672f49d1f79ec368a7b0ad5458aa469c3
Tiago <tiago.venceslau@pdmfc.com>
2021-05-28 11:28:03 +0100

    refs #13 - removes more obsolete code

--- fgt-dsu-wizard/commands/index.js
+++ fgt-dsu-wizard/commands/index.js
@@ -4,16 +4,11 @@
 module.exports = {
     setSSI: require('../../pdm-dsu-toolkit/commands/setSSI'),
     createParticipantSSI: require("./setParticipantSSI").createParticipantSSI,
-    createParticipantConstSSI: require("./setParticipantConstSSI").createParticipantConstSSI,
-    createOrderingPartnerSSI: require("./setOrderingPartnerSSI").createOrderingPartnerSSI,
     createOrderLineSSI: require("./setOrderLineSSI").createOrderLineSSI,
-    createOrderLinesSSI: require("./setOrderLinesSSI").createOrderLinesSSI,
     createOrderSSI: require("./setOrderSSI").createOrderSSI,
     createStatusSSI: require('./setStatusSSI').createStatusSSI,
     createBatchSSI: require("./setBatchSSI").createBatchSSI,
     createProductSSI: require("./setProductSSI").createProductSSI,
-    createSendingPartnerSSI: require("./setSendingPartnerSSI").createSendingPartnerSSI,
     createShipmentLineSSI: require("./setShipmentLineSSI").createShipmentLineSSI,
-    createShipmentLinesSSI: require("./setShipmentLinesSSI").createShipmentLinesSSI,
-    createShipmentSI: require("./setShipmentSSI").createShipmentSSI
+    createShipmentSSI: require("./setShipmentSSI").createShipmentSSI
 }
