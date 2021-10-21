4452d3c75a17cfaca8e293147b9b0bccbf70427e
Vítor Nogueira <vitor.nogueira@pdmfc.com>
2021-10-21 11:23:15 +0100

    refs #36 Batch Manager - scheduling bug fix

--- fgt-dsu-wizard/managers/BatchManager.js
+++ fgt-dsu-wizard/managers/BatchManager.js
@@ -105,39 +105,47 @@ class BatchManager extends Manager{
             const record = keySSI.getIdentifier();
             const dbKey = self._genCompostKey(gtin, batch.batchNumber);
 
+            const dbAction = function(dbKey, record, gtin, batch, product, callback){
+
+                self.insertRecord(dbKey, self._indexItem(gtin, batch, record), (err) => {
+                    if(err){
+                        console.log(`Could not inset record with gtin ${gtin} and batch ${batch.batchNumber} on table ${self.tableName}`);
+                        return cb(err);
+                    }
+                    const path =`${self.tableName}/${dbKey}`;
+                    console.log(`batch ${batch.batchNumber} created stored at '${path}'`);
+    
+                    self.batchAllow(self.stockManager);
+    
+                    self.stockManager.manage(product, batch, (err) => {
+                        if(err){
+                            console.log(`Error Updating Stock for ${product.gtin} batch ${batch.batchNumber}: ${err.message}`);
+                            return cb(err);
+                        }
+                        console.log(`Stock for ${product.gtin} batch ${batch.batchNumber} updated`);
+    
+                        self.batchDisallow(self.stockManager);
+    
+                        self.commitBatch((err) => {
+                            if(err)
+                                return cb(err);
+                            callback(undefined, keySSI, path);
+                        });                
+                    });
+                });
+
+
+            }
+
             try {
                 self.beginBatch();
             } catch (e){
-                return self.batchSchedule(() => self._indexItem.call(self, ...props));
+                return self.batchSchedule(() => self.dbAction.call(self, dbKey, record, gtin, batch, product, callback));
                 //return callback(e);
             }
 
-            self.insertRecord(dbKey, self._indexItem(gtin, batch, record), (err) => {
-                if(err){
-                    console.log(`Could not inset record with gtin ${gtin} and batch ${batch.batchNumber} on table ${self.tableName}`);
-                    return cb(err);
-                }
-                const path =`${self.tableName}/${dbKey}`;
-                console.log(`batch ${batch.batchNumber} created stored at '${path}'`);
+            dbAction(dbKey, record, gtin, batch, product, callback);
 
-                self.batchAllow(self.stockManager);
-
-                self.stockManager.manage(product, batch, (err) => {
-                    if(err){
-                        console.log(`Error Updating Stock for ${product.gtin} batch ${batch.batchNumber}: ${err.message}`);
-                        return cb(err);
-                    }
-                    console.log(`Stock for ${product.gtin} batch ${batch.batchNumber} updated`);
-
-                    self.batchDisallow(self.stockManager);
-
-                    self.commitBatch((err) => {
-                        if(err)
-                            return cb(err);
-                        callback(undefined, keySSI, path);
-                    });                
-                });
-            });
         });
     }
 
