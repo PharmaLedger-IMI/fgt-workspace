ee5d6dd474962f81988d8fc7dfdc3dcf29943bca
Pedro Costa <pedro.c.costa@pdmfc.com>
2021-10-25 11:34:14 -0300

    refs #40 - add stock management service from partners

--- fgt-dsu-wizard/managers/ParticipantManager.js
+++ fgt-dsu-wizard/managers/ParticipantManager.js
@@ -62,6 +62,22 @@ class ParticipantManager extends BaseManager{
         // We use the body because popovers are attached to the doc, not the element
         const listenerElement = controller.element.closest('body');
 
+        listenerElement.addEventListener(EVENTS.STOCK_TRACE, async (evt) => {
+            const sendError = async function(msg, err){
+                await loader.dismiss();
+                controller.showErrorToast(msg, err);
+            }
+            const { gtin, batch, manufName } = evt.detail;
+            const loader = controller._getLoader(`Requesting stock from Partners for ${gtin} Batch: ${batch}`);
+            await loader.present();
+            self.stockManager.getStockTraceability(manufName, gtin, batch, async (err, stockTrace) => {
+                if (err)
+                    return await sendError(err)
+                await loader.dismiss();
+                console.log('# StockManagement stockTrace=', stockTrace)
+            })
+        })
+
         listenerElement.addEventListener(EVENTS.TRACK.REQUEST, async (evt) => {
             evt.preventDefault();
             evt.stopImmediatePropagation();
