12eac01f935bbd90e11f5a02a75000f0c68ba386
Vítor Nogueira <vitor.nogueira@pdmfc.com>
2021-10-29 18:57:35 +0100

    refs #42 initial implementation working

--- fgt-wholesaler-ssapp/code/scripts/controllers/NotificationsController.js
+++ fgt-wholesaler-ssapp/code/scripts/controllers/NotificationsController.js
@@ -4,36 +4,27 @@ import {LocalizedController, EVENT_REFRESH, HistoryNavigator} from "../../assets
  * List all the orders, and allows the creation of new orders.
  */
 export default class NotificationsController extends LocalizedController {
-    initializeModel = () => ({
-       
-    }); // uninitialized blank model
+    initializeModel = () => ({});
 
     constructor(...args) {
         super(false, ...args);
         super.bindLocale(this, "notifications");
-        this.model = this.initializeModel();
         const wizard = require('wizard');
 
         const participantManager = wizard.Managers.getParticipantManager();
-        //this.notificationManager = wizard.Managers.getNotificationManager(participantManager);
-        this.table = this.element.querySelector('pdm-ion-table');
-        // HistoryNavigator.registerTab({
-        //     'tab-issued-orders': this.translate('title')
-        // })
-
+        this.notificationManager = wizard.Managers.getNotificationManager(participantManager);
+        this.notificationManager.bindController(this);
         let self = this;
-        // the HomeController takes care of sending refresh events for each tab.
-        self.on(EVENT_REFRESH, (evt) => {
-            console.log(evt);
+        this.model = this.initializeModel();
+        HistoryNavigator.registerTab({
+            'tab-notifications': this.translate('title')
+        })
+
+        this.on(EVENT_REFRESH, (evt) => {
             evt.preventDefault();
             evt.stopImmediatePropagation();
-            self.table.refresh();
+            self.element.querySelector('pdm-ion-table').refresh();
         }, {capture: true});
-
-        // // pressing "NEW" to create a new Issued Order
-        // self.onTagClick("new-issued-order", () => {
-        //     self.navigateToTab('tab-order');
-        // });
     }
 }
 
