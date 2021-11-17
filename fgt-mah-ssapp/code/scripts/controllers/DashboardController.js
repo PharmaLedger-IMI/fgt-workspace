4d6a6d8c1c485822c89073a70351fbc7b23dd19e
Pedro Costa <pedro.c.costa@pdmfc.com>
2021-11-17 10:41:51 -0300

    refs #32 - add cache functionality to update dashboard charts

--- fgt-mah-ssapp/code/scripts/controllers/DashboardController.js
+++ fgt-mah-ssapp/code/scripts/controllers/DashboardController.js
@@ -14,7 +14,8 @@ export default class DashboardController extends LocalizedController {
             {id: 'requesterId', label: 'Requester', props: {size: '4'}},
             {id: 'status', label: 'Status', props: {size: '3'}},
             {id: 'days', label: 'Last Update', props: {size: '2'}},
-        ])
+        ]),
+        lastUpdate: new Date().toLocaleString()
     });
 
     constructor(...args) {
@@ -31,126 +32,119 @@ export default class DashboardController extends LocalizedController {
         this.stockManager = wizard.Managers.getStockManager(participantManager);
 
         const self = this;
+        self.updateAllCharts();
 
-        self.updateStockChart({
-            labels: ["Diflucan", "Depo Medrol", "Ventolin", "Benlysta", "Fiasp", "Novolin", "Adalat"],
-            monthlyAvg: [1091, 2662, 1650, 3144, 3721, 3160, 3775],
-            productsInStock: [1591, 1962, 2150, 2844, 3521, 3660, 4175]
-        })
-
-        self.updateShipmentsChart({
-            labels: ["Created", "on Hold", "for Pickup", "in Transit", "Delivered", "Pending Conf.","Recall"],
-            shipmentsQtyByStatus: [1251, 2561, 958, 3690, 1305, 2391, 105]
-        })
-
-        self.on(EVENT_REFRESH, (evt) => {
-            evt.preventDefault();
-            evt.stopImmediatePropagation();
-
-            // update sale chart
-            this.stockManager.getAll(true, async (err, products) => {
-                if(err)
-                    console.error(err)
-
-                let index = 0;
-                products.reduce((accum, product) => {
-                    const {gtin, name} = product;
-                    accum[name] = {x: name, gtin: gtin, mah: 0, whs: 0, pha: 0, total: 0};
-                    self.stockManager.getStockTraceability(gtin, {manufName: "MAH135315170"}, (err, stockTrace) => {
-                        const {partnersStock} = stockTrace;
-                        Object.keys(partnersStock).map(key => {
-                            const value = partnersStock[key];
-                            if (key.startsWith('PHA')) {
-                                accum[name].pha += value.dispatched
-                            } else if (key.startsWith('WHS')) {
-                                accum[name].mah += (value.inStock + value.dispatched)
-                                accum[name].whs += value.dispatched
-                            }
-                        })
-                        accum[name].total = accum[name].mah + accum[name].whs + accum[name].pha;
-                        index += 1
-                        if (index === products.length)
-                            this.updateSaleChart({data: Object.values(accum)})
-                    })
-                    return accum;
-                }, {})
-            })
-
-            // update stock chart
-            this.stockManager.getAll(true, (err, stock) => {
-                if(err)
-                    console.error(err)
-                const stockManagement = stock.reduce((accum, product) => {
-                    accum[product.name] = product.quantity
-                    return accum;
-                }, {})
-                const sortStockManagement = Object.entries(stockManagement)
-                    .sort(([,a],[,b]) => a-b)
-                    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
-
-                self.updateStockChart({
-                    labels: Object.keys(sortStockManagement),
-                    monthlyAvg: Object.values(sortStockManagement).map(v => (Math.random() + 0.5) * v),
-                    productsInStock: Object.values(sortStockManagement)
-                })
-            })
-
-            // update shipment chart
-            self.issuedShipmentManager.getAll(true, (err, issuedShipments) => {
-                if (err)
-                    return console.error(err)
-
-                const statusDictionary = {
-                    created: 'Created',
-                    hold: 'on Hold',
-                    pickup: 'for Pickup',
-                    transit: 'in Transit',
-                    delivered: 'Delivered',
-                    received: 'Pending Conf.',
-                    recall: 'Recall'
-                }
-                const shipmentsChartTable = []
-                // initialize with zero for each status
-                const shipments = Object.values(statusDictionary).reduce((acc, statusValue) => {
-                    acc[statusValue] = 0;
-                    return acc
-                }, {})
-                const shipmentsQtyByStatus = issuedShipments.reduce((accum, curr) => {
-                    // if confirmed, the order has been completed
-                    if (curr.status.status !== 'confirmed') {
-                        const statusLabel = statusDictionary[curr.status.status]
-
-                        const timestampDiffFromNow = (timestamp) => {
-                            let delta = (Date.now() - timestamp) / 1000; // delta and transform to seconds
-                            const days = Math.floor(delta / 86400); // 24*60*60 - seconds -> days
-                            delta -= (days * 86400);
-                            const hours = Math.floor(delta / 3600) % 24;
-                            delta -= (hours * 3600);
-                            const min = Math.floor(delta / 60) % 60;
-                            return (days > 0 ? `${days}d` : '') + (hours > 0 ? `${hours}h` : '') + (min > 0 ? `${min}m` : '');
-                        }
-
-                        const lastUpdate = curr.status.log[curr.status.log.length - 1]; // in timestamp
-                        shipmentsChartTable.push({
-                            shipmentId: curr.shipmentId,
-                            requesterId: curr.requesterId,
-                            status: statusLabel,
-                            days: timestampDiffFromNow(lastUpdate.split(' ')[1].trim())
-                        })
-                        accum[statusLabel] += 1; // add +1 shipment qty
-                    }
-                    return accum;
-                }, shipments)
-
-                this.updateShipmentsChart({
-                    labels: Object.keys(shipmentsQtyByStatus),
-                    shipmentsQtyByStatus: Object.values(shipmentsQtyByStatus)
-                })
-                self.model.shipmentsChartTable = JSON.stringify(shipmentsChartTable)
-            })
-
-        }, {capture: true});
+        self.onTagEvent('update-dashboard', 'click', () => {
+            self.updateAllCharts();
+        });
         console.log('DashboardController Initialized');
+    }
+
+    updateAllCharts() {
+        const self = this;
+        self.model.lastUpdate = new Date().toLocaleString()
+        // update sale chart
+        this.stockManager.getAll(true, async (err, products) => {
+            if(err)
+                console.error(err)
+
+            let index = 0;
+            products.reduce((accum, product) => {
+                const {gtin, name} = product;
+                accum[name] = {x: name, gtin: gtin, mah: 0, whs: 0, pha: 0, total: 0};
+                self.stockManager.getStockTraceability(gtin, {manufName: "MAH135315170"}, (err, stockTrace) => {
+                    const {partnersStock} = stockTrace;
+                    Object.keys(partnersStock).map(key => {
+                        const value = partnersStock[key];
+                        if (key.startsWith('PHA')) {
+                            accum[name].pha += value.dispatched
+                        } else if (key.startsWith('WHS')) {
+                            accum[name].mah += (value.inStock + value.dispatched)
+                            accum[name].whs += value.dispatched
+                        }
+                    })
+                    accum[name].total = accum[name].mah + accum[name].whs + accum[name].pha;
+                    index += 1
+                    if (index === products.length)
+                        this.updateSaleChart({data: Object.values(accum)})
+                })
+                return accum;
+            }, {})
+        })
+
+        // update stock chart
+        this.stockManager.getAll(true, (err, stock) => {
+            if(err)
+                console.error(err)
+            const stockManagement = stock.reduce((accum, product) => {
+                accum[product.name] = product.quantity
+                return accum;
+            }, {})
+            const sortStockManagement = Object.entries(stockManagement)
+                .sort(([,a],[,b]) => a-b)
+                .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+            self.updateStockChart({
+                labels: Object.keys(sortStockManagement),
+                monthlyAvg: Object.values(sortStockManagement).map(v => (Math.random() + 0.5) * v),
+                productsInStock: Object.values(sortStockManagement)
+            })
+        })
+
+        // update shipment chart
+        self.issuedShipmentManager.getAll(true, (err, issuedShipments) => {
+            if (err)
+                return console.error(err)
+
+            const statusDictionary = {
+                created: 'Created',
+                hold: 'on Hold',
+                pickup: 'for Pickup',
+                transit: 'in Transit',
+                delivered: 'Delivered',
+                received: 'Pending Conf.',
+                recall: 'Recall'
+            }
+            const shipmentsChartTable = []
+            // initialize with zero for each status
+            const shipments = Object.values(statusDictionary).reduce((acc, statusValue) => {
+                acc[statusValue] = 0;
+                return acc
+            }, {})
+            const shipmentsQtyByStatus = issuedShipments.reduce((accum, curr) => {
+                // if confirmed, the order has been completed
+                if (curr.status.status !== 'confirmed') {
+                    const statusLabel = statusDictionary[curr.status.status]
+
+                    const timestampDiffFromNow = (timestamp) => {
+                        let delta = (Date.now() - timestamp) / 1000; // delta and transform to seconds
+                        const days = Math.floor(delta / 86400); // 24*60*60 - seconds -> days
+                        delta -= (days * 86400);
+                        const hours = Math.floor(delta / 3600) % 24;
+                        delta -= (hours * 3600);
+                        const min = Math.floor(delta / 60) % 60;
+                        return (days > 0 ? `${days}d` : '') + (hours > 0 ? `${hours}h` : '') + (min > 0 ? `${min}m` : '');
+                    }
+
+                    const lastUpdate = curr.status.log[curr.status.log.length - 1]; // in timestamp
+                    shipmentsChartTable.push({
+                        shipmentId: curr.shipmentId,
+                        requesterId: curr.requesterId,
+                        status: statusLabel,
+                        days: timestampDiffFromNow(lastUpdate.split(' ')[1].trim())
+                    })
+                    accum[statusLabel] += 1; // add +1 shipment qty
+                }
+                return accum;
+            }, shipments)
+
+            this.updateShipmentsChart({
+                labels: Object.keys(shipmentsQtyByStatus),
+                shipmentsQtyByStatus: Object.values(shipmentsQtyByStatus)
+            })
+            self.model.shipmentsChartTable = JSON.stringify(shipmentsChartTable)
+        })
+
     }
 
     updateSaleChart(metadata, options) {
