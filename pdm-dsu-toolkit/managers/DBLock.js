a3176e18abbb713be38c500c4250dba78b92b788
jpsl <joao.luis@pdmfc.com>
2021-12-03 15:56:03 +0000

    Revert "#55 - more console.log on DBLock"
    This reverts commit c6ecfd1ad497a92858ab94bed1a176320ad8dacd.

--- pdm-dsu-toolkit/managers/DBLock.js
+++ pdm-dsu-toolkit/managers/DBLock.js
@@ -71,16 +71,14 @@ class DBLock {
      * @throws {Error} error when a batch operation is already in progress
      */
     beginBatch(tableName){
-        console.log(`DBLock.beginBatch(${tableName})`);
+
         if (tableName in this._allows)
             tableName = this._allows[tableName]
 
         this._cache[tableName] = this._cache[tableName] || -1;
 
         if (this._cache[tableName] === -1){
-            console.log(`_storage.beginBatch() start`);
             this._storage.beginBatch.call(this._storage);
-            console.log(`_storage.beginBatch() done`);
             this._cache[tableName] = 1;
         } else {
             this._cache[tableName] += 1;
@@ -111,7 +109,6 @@ class DBLock {
      * @param {(err) => void} callback
      */
     commitBatch(tableName, force, callback){
-        console.log(`DBLock.commitBatch(${tableName},${force})`);
         if (!callback){
             callback = force;
             force = false;
@@ -152,7 +149,6 @@ class DBLock {
      * @param {(err) => void} callback
      */
     cancelBatch(tableName, callback){
-        console.log(`DBLock.cancelBatch(${tableName})`);
         if (this._cache[tableName] > 0){
             delete this._cache[tableName];
             return this._storage.cancelBatch.call(this._storage, callback);
