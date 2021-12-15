3d9fc36e7c3babcef530795ce094258b715b012a
Vítor Nogueira <vitor.nogueira@pdmfc.com>
2021-12-15 12:20:27 +0000

    refs #56 Message manager integration test created

---
+++ tests/integration/managers/message-manager/message-manager.test.js
@@ -0,0 +1,227 @@
+/* SKIP TEST */
+
+// process.exit(0); //uncomment to skip test ... 
+
+//################################################################################################
+
+/* Test Configurations */
+
+const testOptions = {
+    noLogs: true, // Prevents from recording logs. (set to false if you want to record them);
+    testName: 'MessageManagerIntegrationTest', // no spaces please. its used as a folder name (change for the unit being tested);
+    timeout: 10000000, // Sets timeout for the test.
+    fakeServer: true, // Decides if fake server is used (change if you want to use fake server);
+    useCallback: true, // Decides if you want to use callback (change if you dont want to use it);
+};
+
+const options = {
+    domain: 'traceability',
+    DID_METHOD: 'ssi:name',
+}
+
+//################################################################################################
+
+/* Fake Server Config*/
+
+const DOMAIN_CONFIG = {
+    anchoring: {
+        type: "FS",
+        option: {
+            enableBricksLedger: false,
+        },
+        commands: {
+            addAnchor: "anchor",
+        },
+    },
+    enable: ["mq"],
+};
+
+const getBDNSConfig = function(folder){
+    return {
+        maxTries: 10,
+        storageFolder: folder,
+        domains: [
+            {
+                name: options.domain,
+                config: DOMAIN_CONFIG,
+            },
+            {
+                name: 'vault',
+                config: DOMAIN_CONFIG,
+            },
+        ],
+    }
+}
+
+//################################################################################################
+
+/* Test Pre-Setup */
+
+process.env.NO_LOGS = testOptions.noLogs;
+
+//################################################################################################
+
+/* Test Imports */
+
+const path = require('path');
+
+const test_bundles_path = path.join('../../../../privatesky/psknode/bundles', 'testsRuntime.js');
+require(test_bundles_path);
+
+const tir = require("../../../../privatesky/psknode/tests/util/tir");
+
+const dc = require("double-check");
+const assert = dc.assert;
+
+const { argParser } = require('../../../../bin/environment/utils');
+
+const opendsu = require("opendsu");
+const w3cDID = opendsu.loadApi('w3cdid');
+const scAPI = opendsu.loadAPI("sc");
+
+
+//################################################################################################
+
+/*General Dependencies*/
+
+const config = argParser(options, process.argv);
+
+//################################################################################################
+
+// /*Specific Dependencies*/
+
+const { getCredentials, APPS } = require('../../../../bin/environment/credentials/credentials3');
+const { getMockParticipantManager } = require('../../../getMockParticipant');
+const { getMessageManager, Message } = require('../../../../pdm-dsu-toolkit/managers/MessageManager');
+
+const apps = {};
+
+// //################################################################################################
+
+/* Test API */
+
+const createAPP = function (app, force, callback) {
+
+    const initialCredentials = getCredentials(app); // Creates Credentials
+
+    const finalCredentials = Object.keys(initialCredentials). reduce((acum, key) => {
+        if(initialCredentials[key].public)
+            acum[key] = initialCredentials[key].secret;
+        
+        return acum;
+    }, {});
+
+    if(!apps['ids'])
+        apps['ids'] = [];
+
+    apps['ids'].push(initialCredentials['id']['secret'])
+
+    getMockParticipantManager(config.domain, finalCredentials, force, (err, participantManager) => {
+        assert.false(err, 'Could not create participant manager: ', err);
+
+        callback(participantManager);
+    })
+
+}
+
+//################################################################################################
+
+/* Run Test */ 
+
+const runTest = function(finishTest){
+    
+    createAPP(APPS.MAH, false, (participantManager) => {
+        
+
+        const messageManagerMAH = getMessageManager(participantManager);
+
+        console.log(apps['ids'][0]);
+
+        messageManagerMAH._getDID(apps['ids'][0], (err, didDoc) => {
+            assert.false(err, 'No DID doc');
+
+            console.log(didDoc.getIdentifier());
+            finishTest();
+        });
+
+
+    
+
+
+
+
+
+        // createAPP(APPS.WHOLESALER, true, (participantManager) => {
+        //     apps['WHS1'] = participantManager;
+
+        //     const messageManagerWHS = getMessageManager(participantManager);
+        //     messageManagerWHS.getOwnDID((err, didDoc) => {
+
+        //         const whsdid = didDoc.getIdentifier().split(':');
+
+        //         const message = new Message('test' , 'hello');
+                
+        //         try{
+
+        //             messageManagerMAH.sendMessage(whsdid[4], message, finishTest);
+                 
+        //         } catch (e) {
+                    
+        //             return finishTest(e);
+
+        //         }
+                            
+        //     })
+          
+        // })            
+           
+    })
+};
+
+//################################################################################################
+
+/*Launch Test*/
+
+const testFinishCallback = function(callback){
+    console.log(`Test ${testOptions.testName} finished successfully`);
+    if (callback)
+        return callback();
+    setTimeout(() => {
+        process.exit(0);
+    }, 1000);
+};
+
+const launchTest = function(callback){
+    const testRunner = function(callback){
+        runTest((err) => {
+            if (err)
+                throw err;
+            testFinishCallback(callback);
+        });
+    };
+
+    const runWithFakeServer = function(callback){
+        dc.createTestFolder(testOptions.testName, async (err, folder) => {
+            await tir.launchConfigurableApiHubTestNodeAsync(getBDNSConfig(folder));
+
+            if (!callback)
+                assert.begin(`Running test ${testOptions.testName}`, undefined, testOptions.timeout);
+            testRunner(callback);
+        });
+    };
+
+    if (testOptions.fakeServer)
+        return runWithFakeServer(callback);
+
+    if (!callback)
+        assert.begin(`Running test ${testOptions.testName}`, undefined, testOptions.timeout);
+    testRunner(callback);
+};
+
+
+if (!testOptions.useCallback)
+    return launchTest();
+    
+assert.callback(testOptions.testName, (testFinished) => {
+    launchTest(testFinished);
+}, testOptions.timeout);
