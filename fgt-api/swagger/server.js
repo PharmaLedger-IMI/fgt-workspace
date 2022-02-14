5f2f36737c60b5b997cee8f9191a0dfbac586134
Pedro Costa <pedro.c.costa@pdmfc.com>
2022-02-14 07:01:27 -0300

    refs #60 - add swagger to compose

--- fgt-api/swagger/server.js
+++ fgt-api/swagger/server.js
@@ -23,10 +23,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 app.get('*', (req, res) => {
-    res.redirect('/api-docs');
+    res.redirect('/');
 });
 
 app.listen(PORT, console.log(`[FGT-API] Swagger API DOC listening on :${PORT} and able to make requests to API: ${API_SERVER}`));
