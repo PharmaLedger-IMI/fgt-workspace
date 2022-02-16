e69968c09efcc5d12844a59e5def6f45142ea658
Tiago PDM <tiago.venceslau@pdmfc.com>
2022-02-16 10:19:32 +0000

    refs #60 - adds condition for docker compose environment

---
+++ docker/api/bootCompose.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+if test -z "$1"; then
+  params="localhost"
+else
+  if test "$1" = "dev"; then
+    params="fgt-${1}.pharmaledger.pdmfc.com"
+  else
+    params="fgt.pharmaledger.pdmfc.com"
+  fi
+fi
+
+echo "Launching docker compose in $params"
+
+DOMAIN=$params docker-compose up -d
