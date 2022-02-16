ec1b4515ba765221bff693b01322adde0b093e1b
Tiago PDM <tiago.venceslau@pdmfc.com>
2022-02-16 16:31:54 +0000

    refs #60 - updates

--- docker/api/bootCompose.sh
+++ docker/api/bootCompose.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -xe
 if test -z "$1"; then
   params=".localhost"
+  swagger=".localhost:8080"
 else
   if test "$1" = "dev"; then
     params="-fgt-${1}.pharmaledger.pdmfc.com"
+    swagger="$params"
   else
     params="-fgt.pharmaledger.pdmfc.com"
+    swagger="$params"
   fi
 fi
 
 echo "Launching docker compose in $params"
 
-DOMAIN=$params docker-compose up -d &
-docker-compose logs -f -t --tail="all"
+DOMAIN=$params SWAGGER=$swagger docker-compose up &
