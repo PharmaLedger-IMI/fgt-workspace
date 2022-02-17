b390fb6c169fe8344a276ca90731defa3b340472
Tiago PDM <tiago.venceslau@pdmfc.com>
2022-02-17 02:31:16 +0000

    refs #60 - working locally again

--- docker/api/bootCompose.sh
+++ docker/api/bootCompose.sh
@@ -2,16 +2,17 @@
 if test -z "$1"; then
   params=".localhost"
   swagger=".localhost:8080"
+  protocol="http"
 else
   if test "$1" = "dev"; then
     params="-fgt-${1}.pharmaledger.pdmfc.com"
-    swagger="$params"
   else
     params="-fgt.pharmaledger.pdmfc.com"
-    swagger="$params"
   fi
+  swagger="$params"
+  protocol=https
 fi
 
 echo "Launching docker compose in $params"
 
-DOMAIN=$params SWAGGER=$swagger docker-compose up -d
+DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol docker-compose up -d
