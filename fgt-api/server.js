4767c3812feb8a2e430c77e9aaacd6f682038c88
Tiago PDM <tiago.venceslau@pdmfc.com>
2022-02-17 00:35:17 +0000

    refs #60 - reverts change

--- fgt-api/server.js
+++ fgt-api/server.js
@@ -9,7 +9,7 @@ if (!ROLE){
     process.exit(0);
 }
 
-console.log(`ENVIRONMENT VARIABLES: ROLE: ${ROLE}`)
+console.log(`ENVIRONMENT VARIABLES: ROLE: ${ROLE}, CREDENTIALS_FILE: ${CREDENTIALS_FILE} and SWAGGER_SERVER: ${SWAGGER_SERVER}`)
 
 function failServerBoot(reason){
     console.error("Server boot failed: " + reason);
@@ -34,13 +34,51 @@ function overWriteCredentialsByRole(){
         path.join(currentPath, "config", `fgt-${getWallet()}-wallet`, "credentials.json"))
 }
 
-function bootAPIServer(){
+async function bootAPIServer(){
     require(path.join(currentPath, "participants", ROLE, "index.js"));
+}
+
+async function bootSwagger(){
+    const YAML = require('yamljs');
+    const express = require('express');
+    const cors = require('cors');
+    const swaggerUi = require('swagger-ui-express');
+
+    const config = {
+        port: 3009,
+        server: SWAGGER_SERVER,
+        path: "./swagger/docs",
+        participant: ROLE.toUpperCase()
+    };
+    const PORT = config.port;
+    const PATH = config.path;
+    const PARTICIPANT = config.participant;
+    const API_SERVER = config.server;
+    console.log('[FGT-API] Swagger load config=', config);
+
+    const swaggerPathResolve = path.resolve(PATH, PARTICIPANT.toUpperCase() + '.yml');
+    const swaggerDocument = YAML.parse(fs.readFileSync(swaggerPathResolve, 'utf8'));
+    swaggerDocument.servers = [{url: API_SERVER}];
+    const options = {
+        customCss: '.swagger-ui .topbar { display: none }'
+    };
+
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+
+    app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
+    app.get('*', (req, res) => {
+        res.redirect('/');
+    });
+
+    app.listen(PORT, console.log(`[FGT-API] Swagger API DOC listening on :${PORT} and able to make requests to API: ${API_SERVER}`));
 }
 
 try {
     overWriteCredentialsByRole();
-    bootAPIServer();
+    Promise.all([bootAPIServer(), bootSwagger()]).then(_ => console.log(`Completed Boot`)).catch(e => failServerBoot(e.message));
 } catch (e){
     failServerBoot(e.message);
 }
