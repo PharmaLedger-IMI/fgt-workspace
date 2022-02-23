1cb1e15354c070533a90b8d0c6af692ccabf5589
jpsl <joao.luis@pdmfc.com>
2022-02-23 11:43:51 +0000

    #30 - startup-noeth-script.sh

--- docker/api/bootCompose.sh
+++ docker/api/bootCompose.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -xe
-command="startup-script.sh"
+if [ "$1" = "help" -o "$1" = "--help" -o "$1" = "-h" -o "$1" = "-?" ]; then
+  echo "Usage:"
+  echo
+  echo "\t $0 [options]"
+  echo
+  echo "In options use keyword eth to turn on BC integration. Use keywork dev for DEV environment. Use keyword tst for TST environment."
+  echo
+  echo "Examples:"
+  echo
+  echo "\t $0"
+  echo
+  echo "\t $0 eth-dev"
+  echo
+  echo "\t $0 tst"
+  exit 0
+fi
 params=".localhost"
 simple="localhost"
 swagger=".localhost:8080"
@@ -16,9 +31,9 @@ elif [[ "$1" = *"tst"* ]]; then
   protocol=https
 fi
 if [[ "$1" = *"eth"* ]]; then
-    command="startup-eth-script.sh"
+    echo "Launching docker compose in $params with ETH"
+    SIMPLE=$simple DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol COMMAND=$command docker-compose -f docker-compose.yml -f docker-compose-eth.yml up -d
+else
+    echo "Launching docker compose in $params without ETH"
+    SIMPLE=$simple DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol COMMAND=$command docker-compose up -d
 fi
-
-echo "Launching docker compose in $params"
-
-SIMPLE=$simple DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol COMMAND=$command docker-compose up -d
