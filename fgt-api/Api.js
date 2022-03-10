1b08cf5900b78e8a84ffad8b56b482743573713c
jpsl <joao.luis@pdmfc.com>
2022-03-10 12:34:05 +0000

    #69 - JSON.parse after Buffer.concat

--- fgt-api/Api.js
+++ fgt-api/Api.js
@@ -54,7 +54,7 @@ const parseRequestBody = function(req, callback){
     req.on('end', () => {
         console.log(`\n(end) data=`, Buffer.concat(data).toString(), '\n');
         try {
-            req.body = data.length ? JSON.parse(data) : {};
+            req.body = data.length ? JSON.parse(Buffer.concat(data)) : {};
         } catch (e) {
             return callback(e);
         }
