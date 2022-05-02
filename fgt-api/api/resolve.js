d1421856f9892d0a86a3a71f57652f81c7bfbc75
Tiago PDM <tiago.venceslau@pdmfc.com>
2022-05-02 10:15:46 +0100

    refs #62 - adds resolver api

--- fgt-api/api/resolve.js
+++ fgt-api/api/resolve.js
@@ -1,4 +1,4 @@
-
+const {ANCHORING_DOMAIN} = require('../../fgt-dsu-wizard/constants');
 const {Api, OPERATIONS} = require('../Api');
 const {BadRequest} = require("../utils/errorHandler");
 
@@ -11,8 +11,8 @@ class ResolverApi extends Api {
     constructor(server, participantManager) {
         super(server, 'resolve', participantManager, [RESOLVER_GET]);
         try {
-            this.productManager = new (require('../../fgt-dsu-wizard/services/ProductService'))("traceability");
-            this.batchManager = new (require('../../fgt-dsu-wizard/services/BatchService'))("traceability");
+            this.productManager = new (require('../../fgt-dsu-wizard/services/ProductService'))(ANCHORING_DOMAIN);
+            this.batchManager = new (require('../../fgt-dsu-wizard/services/BatchService'))(ANCHORING_DOMAIN);
         } catch (e) {
             throw new Error(`Could not get ${this.endpoint}Manager: ${e}`);
         }
