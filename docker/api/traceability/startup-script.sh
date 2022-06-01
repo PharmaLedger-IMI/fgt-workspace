336f6a6c25c709a6edbd81301bf98ead778a46df
jpsl <joao.luis@pdmfc.com>
2022-06-01 14:00:02 +0100

    #74 - fix config path for tracebility startup-script.sh

--- docker/api/traceability/startup-script.sh
+++ docker/api/traceability/startup-script.sh
@@ -2,5 +2,5 @@
 # use /bin/sh to be compatible with Alpine / Busybox
 # This script expects cwd to be /fgt-workspace
 rm -rf apihub-root/external-volume/config
-cp -r docker/api/config apihub-root/external-volume/config
+cp -r docker/api/traceability/config apihub-root/external-volume/config
 npm run switch-to-simul-chain ; npm run server & ( sleep 5s ; npm run build-all ; tail -f /dev/null )
