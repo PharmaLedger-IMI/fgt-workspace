040a5256cf7d0dfc84632d201df4384ac2f10ba1
jpsl <joao.luis@pdmfc.com>
2022-06-01 12:55:29 +0100

    #74 - something is wrong with startup-script. Trying again.

--- docker/api/startup-script.sh
+++ docker/api/startup-script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # use /bin/sh to be compatible with Alpine / Busybox
+# This script expects cwd to be /fgt-workspace
 rm -rf apihub-root/external-volume/config
 cp -r docker/api/config apihub-root/external-volume/config
-cd /fgt-workspace ; npm run server & ( sleep 5s ; npm run build-all ; cd fgt-api ; npm run boot-api )
+npm run server & ( sleep 5s ; npm run build-all ; cd fgt-api ; npm run boot-api )
--- docker/api/traceability/startup-script.sh
+++ docker/api/traceability/startup-script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # use /bin/sh to be compatible with Alpine / Busybox
+# This script expects cwd to be /fgt-workspace
 rm -rf apihub-root/external-volume/config
 cp -r docker/api/config apihub-root/external-volume/config
-cd /fgt-workspace ; npm run switch-to-simul-chain ; npm run server & ( sleep 5s ; npm run build-all ; tail -f /dev/null )
+npm run switch-to-simul-chain ; npm run server & ( sleep 5s ; npm run build-all ; tail -f /dev/null )
