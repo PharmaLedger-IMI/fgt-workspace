2e11016dee960a5281b23d0c52bd746aea040308
Pedro Costa <pedro.c.costa@pdmfc.com>
2022-07-12 01:24:45 -0300

    refs #89 - update

--- docker/api/startup-script.sh
+++ docker/api/startup-script.sh
@@ -3,4 +3,4 @@
 # This script expects cwd to be /fgt-workspace
 rm -rf apihub-root/external-volume/config
 cp -r docker/api/config apihub-root/external-volume/config
-npm run server & ( sleep 5s ; npm run build-all ; cd fgt-api ; npm run boot-api )
+npm run server & ( sleep 5s ; npm run build-all ; npm run build-dsu-explorer ; cd fgt-api ; npm run boot-api )
