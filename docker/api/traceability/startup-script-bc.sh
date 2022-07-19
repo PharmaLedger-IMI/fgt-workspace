fdd7c031156162bbc17cb0becf7b6347710fd1aa
Pedro Costa <pedro.c.costa@pdmfc.com>
2022-07-19 12:39:36 -0300

    refs #89 - fix build

--- docker/api/traceability/startup-script-bc.sh
+++ docker/api/traceability/startup-script-bc.sh
@@ -3,4 +3,4 @@
 # This script expects cwd to be /fgt-workspace
 rm -rf apihub-root/external-volume/config
 cp -r docker/api/config apihub-root/external-volume/config
-npm run switch-to-test-chain ; npm run server & ( sleep 5s ; npm run build-all ; tail -f /dev/null )
+npm run switch-to-test-chain ; npm run server & ( sleep 5s ; npm run build-all; npm run build-dsu-explorer ; tail -f /dev/null )
