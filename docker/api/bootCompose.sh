7bfdbfdb3fd357d0b115b9b989cdafc9ff6e06d8
Tiago PDM <tiago.venceslau@pdmfc.com>
2022-11-09 14:21:13 +0000

    refs #123 - updates bootCompose

--- docker/api/bootCompose.sh
+++ docker/api/bootCompose.sh
@@ -19,21 +19,24 @@ params=".localhost"
 simple="localhost"
 swagger=".localhost:8080"
 protocol="http"
+environment="local"
 if [[ "$1" = *"dev"* ]]; then
   params="-fgt-dev.pharmaledger.pdmfc.com"
   simple="fgt-dev.pharmaledger.pdmfc.com"
+  environment="dev"
   swagger="$params"
   protocol=https
 elif [[ "$1" = *"tst"* ]]; then
   params="-fgt.pharmaledger.pdmfc.com"
   simple="fgt.pharmaledger.pdmfc.com"
+  environment="tst"
   swagger="$params"
   protocol=https
 fi
 if [[ "$1" = *"eth"* || "$1" = *"bc"* ]]; then
     echo "Launching docker compose in $params with ETH"
-    SIMPLE=$simple DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol COMMAND=$command docker-compose -f docker-compose.yml -f docker-compose-bc.yml up -d
+    SIMPLE=$simple DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol ENVIRONMENT=$environment COMMAND=$command docker-compose -f docker-compose.yml -f docker-compose-bc.yml up -d
 else
     echo "Launching docker compose in $params without ETH"
-    SIMPLE=$simple DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol COMMAND=$command docker-compose up -d
+    SIMPLE=$simple DOMAIN=$params SWAGGER=$swagger PROTOCOL=$protocol ENVIRONMENT=$environment COMMAND=$command docker-compose up -d
 fi
