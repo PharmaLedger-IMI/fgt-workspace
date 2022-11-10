09c6d25a96604aa7fb2a7eab64898297b61a0017
Tiago PDM <tiago.venceslau@pdmfc.com>
2022-11-10 09:49:19 +0000

    refs #123 - bug fix on startup script

--- docker/api/startup-script.sh
+++ docker/api/startup-script.sh
@@ -6,4 +6,4 @@ cp -r docker/api/config apihub-root/external-volume/config
 
 echo "Starting server and building under ${ROLE}, ${FGT_API_CREDENTIALS}, ${SWAGGER_SERVER}"
 
-npm update-docker-apihub-config && npm run server & ( sleep 5s ; npm run build-all ; npm run build-dsu-explorer ; cd fgt-api ; npm run boot-api )
+npm run update-docker-apihub-config && npm run server & ( sleep 5s ; npm run build-all ; npm run build-dsu-explorer ; cd fgt-api ; npm run boot-api )
