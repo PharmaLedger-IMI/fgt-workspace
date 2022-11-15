035d6d8478a6730f96a47a24a3cc0ebe26b74f3f
jpsl <joao.luis@pdmfc.com>
2022-11-15 16:43:14 +0000

    #126 #74 - attempt v0.11.4

---
+++ blockchain-patch/apply/privatesky/psknode/core/utils/pingpongFork.js
@@ -0,0 +1,91 @@
+const PING = "PING";
+const PONG = "PONG";
+
+module.exports.fork = function pingPongFork(modulePath, args, options){
+    const child_process = require("child_process");
+    const defaultStdio = ["inherit", "inherit", "inherit", "ipc"];
+
+    if(!options){
+        options = {stdio: defaultStdio};
+    }else{
+        if(typeof options.stdio === "undefined"){
+            options.stdio = defaultStdio;
+        }
+
+        let stdio = options.stdio;
+        if(stdio.length<3){
+            for(let i=stdio.length; i<4; i++){
+                stdio.push("inherit");
+            }
+            stdio.push("ipc");
+        }
+    }
+
+    let child = child_process.fork(modulePath, args, options);
+
+    child.on("message", (message)=>{
+        if(message === PING){
+            child.send(PONG);
+        }
+    });
+
+    return child;
+};
+
+module.exports.enableLifeLine = function(timeout){
+
+    if(typeof process.send === "undefined"){
+        console.log("\"process.send\" not found. LifeLine mechanism disabled!");
+        return;
+    }
+
+    let lastConfirmationTime;
+    const interval = timeout || 10 * 60 * 1000;
+
+    // this is needed because new Date().getTime() has reduced precision to mitigate timer based attacks
+    // for more information see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTime
+    const roundingError = 101;
+
+    function sendPing(){
+        try {
+            process.send(PING);
+        } catch (e) {
+            //console.log('Parent is not available, shutting down');
+            //exit(1)
+        }
+    }
+
+    process.on("message", function (message){
+        if(message === PONG){
+            lastConfirmationTime = new Date().getTime();
+        }
+    });
+
+    function exit(code){
+        setTimeout(()=>{
+            process.exit(code);
+        }, 0);
+    }
+
+    const exceptionEvents = ["SIGINT", "SIGUSR1", "SIGUSR2", "SIGTERM", "SIGHUP"];
+    let killingSignal = false;
+    for(let i=0; i<exceptionEvents.length; i++){
+        process.on(exceptionEvents[i], (event, code)=>{
+            killingSignal = true;
+            clearInterval(timeoutInterval);
+            //console.log(`Caught event type [${exceptionEvents[i]}]. Shutting down...`, code, event);
+            //exit(code);
+        });
+    }
+
+    const timeoutInterval = setInterval(function(){
+        const currentTime = new Date().getTime();
+
+        if(typeof lastConfirmationTime === "undefined" || currentTime - lastConfirmationTime < interval + roundingError && !killingSignal){
+            sendPing();
+        }else{
+            //console.log("Parent process did not answer. Shutting down...", process.argv, killingSignal);
+            //exit(1);
+        }
+    }, interval);
+};
--- docker/api/traceability/startup-script.sh
+++ docker/api/traceability/startup-script.sh
@@ -3,4 +3,5 @@
 # This script expects cwd to be /fgt-workspace
 rm -rf apihub-root/external-volume/config
 cp -r docker/api/traceability/config apihub-root/external-volume/config
+cp -r blockchain-patch/apply/privatesky/psknode/core/utils/pingpongFork.js privatesky/psknode/core/utils/pingpongFork.js
 npm run switch-to-simul-chain ; npm run server & ( sleep 5s ; npm run build-all; npm run build-dsu-explorer ; tail -f /dev/null )
